734493d55622f07c7569f59ee8ca2e45e73f2347
notPHPuser <artem2005dm@mail.ru>
2024-03-13 01:03:31 +0300

    bank

--- frontend/src/components/someText/SomeText.jsx
+++ frontend/src/components/someText/SomeText.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './SomeText.css';
+import { Link } from 'react-router-dom';
 
 function SomeText() {
   return (
     <>
+      <h1 className='goodText'>Нам доверяют более 20 млн клиентов</h1>
       <div className='WeAreGood'>
-        <h1 className='goodText'>Нам доверяют более 20 млн клиентов</h1>
-        <div className='goodItem'></div>
+        <div className='goodItem'>
+          <p className='p_first_goodItem'>
+            AdbK обладатель пяти наград премии <br /> "Банк года" Банки.ру
+          </p>
+          <p className='p_second_goodItem'>
+            Adbk был признан лучшим в следующих категориях: <br />
+            -"Забота о клиенте" <br /> -"Digital-банк года" <br /> -"Народный рейтинг страховых
+            компаний"
+          </p>
+          <Link to='/' className='btn_goodItem'>
+            Подробнее
+          </Link>
+        </div>
       </div>
     </>
   );
